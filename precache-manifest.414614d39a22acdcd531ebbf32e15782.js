9e7d427fbe20d588840812a14525f732aeed44aa
freeman <freeman23@bigmir.net>
2019-04-25 00:04:16 +0300

    Updates

---
+++ precache-manifest.414614d39a22acdcd531ebbf32e15782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d06490e05954047314904c465bf3f8",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "6c952bdb0d26d03c3964",
+    "url": "/react-todo-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c6788d4660a3bc38a7ad",
+    "url": "/react-todo-list/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "6c952bdb0d26d03c3964",
+    "url": "/react-todo-list/static/js/2.21307c79.chunk.js"
+  },
+  {
+    "revision": "c6788d4660a3bc38a7ad",
+    "url": "/react-todo-list/static/js/main.55ed1449.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
